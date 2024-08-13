83b2c0c34c9b758d80675c24cbbab79d894d12ac
Lars Holmberg <lars@getlocust.com>
2024-08-13 11:40:08 +0200

    Improve test cases

--- twosum_test.py
+++ twosum_test.py
@@ -3,7 +3,7 @@ import unittest
 from twosum import two_sum
 
 
-class TestStringMethods(unittest.TestCase):
+class TestTwoSum(unittest.TestCase):
     def test_example1(self):
         self.assertEqual(two_sum([2, 7, 11, 15], 9), [0, 1])
 
@@ -12,3 +12,9 @@ class TestStringMethods(unittest.TestCase):
 
     def test_example3(self):
         self.assertEqual(two_sum([3, 3], 6), [0, 1])
+
+    # larger numbers in response to avoid completely broken implementation working by accident
+    def test_example4(self):
+        self.assertEqual(two_sum([1, 4, 12, 5], 9), [1, 3])
+if __name__ == "__main__":
+    unittest.main()  # or you can run it using python -m unittest twosum_test.py
